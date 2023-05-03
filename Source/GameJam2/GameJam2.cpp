66bfc1c453bc84e14b06716c8bb1dd10020a7a75
NUNO <U2068978@unimail.hud.ac.uk>
2023-05-03 16:43:11 +0100

    Level_03

---
+++ Source/GameJam2/GameJam2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameJam2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJam2, "GameJam2" );
